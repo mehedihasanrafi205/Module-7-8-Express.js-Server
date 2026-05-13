114b4ec9182f1c22914398c1a53d306121de4e5c
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2026-05-13 21:56:03 +0600

    Executing Pool and creating tables

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,28 @@ const pool = new Pool({
     "postgresql://neondb_owner:npg_Emd9sU6qnoRu@ep-misty-cherry-aqwyhnpr-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
 
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20),
+        email VARCHAR(20) UNIQUE NOT NULL,
+        password VARCHAR(20) NOT NULL,
+        is_active BOOLEAN DEFAULT true,
+        age INT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+      `);
+    console.log("Database Connected Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initDB();
+
 app.get("/user", (req: Request, res: Response) => {
   res.status(200).json({
     message: "Express Server",
