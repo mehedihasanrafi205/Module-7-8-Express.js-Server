a04e82691df7e634df0655f967aa74c66cd894fb
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2026-05-18 16:39:23 +0600

    express middleware

--- src/app.ts
+++ src/app.ts
@@ -4,18 +4,24 @@ import express, {
   type Response,
 } from "express";
 
-import { Pool } from "pg";
-import config from "./config";
-import { initDB, pool } from "./db";
 import { userRoute } from "./modules/user/user.route";
 import { ProfileRoute } from "./modules/profile/profile.route";
 import { authRoute } from "./modules/auth/auth.route";
+
+import fs from "fs";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log("Method - URL - Time:", req.method, req.url, Date.now());
+  const log = `\nMethod -> ${req.method} - Time -> ${Date.now()} - URL -> ${req.url}\n`;
+  fs.appendFile("logger.txt", log, (err) => {});
+  next();
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
